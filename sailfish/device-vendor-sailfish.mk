0fa35c0281ef49833687dcf43acbff9553b6f0ce
BeansTown106 <nbenis106@gmail.com>
2017-09-21 02:45:24 -0400

    sailfish: proprietary blobs for 8.0.0 (OPR3.170623.007)

---
+++ sailfish/device-vendor-sailfish.mk
@@ -0,0 +1,63 @@
+# Copyright (C) 2017 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, vendor/google_devices/sailfish/sailfish-vendor-blobs.mk)
+
+# Source built
+PRODUCT_PACKAGES += \
+    PresencePolling \
+    RcsService
+
+# Prebuilt /apps
+PRODUCT_PACKAGES += \
+    Tycho \
+    VZWAPNLib
+
+# Prebuilt /framework
+PRODUCT_PACKAGES += \
+    VerizonUnifiedSettings
+
+# Prebuilt /priv-app
+PRODUCT_PACKAGES += \
+    AppDirectedSMSService \
+    CarrierServices \
+    CNEService \
+    ConnMO \
+    DCMO \
+    DiagMon \
+    DMService \
+    GCS \
+    GoogleCarrierConfig \
+    HotwordEnrollmentWCD9335 \
+    LLKAgent \
+    OBDM_Permissions \
+    obdm_stub \
+    OemDmTrigger \
+    OTAConfigPrebuilt \
+    qcrilmsgtunnel \
+    Showcase \
+    SprintDM \
+    SprintHM \
+    TetheringEntitlement \
+    VZWAPNService \
+    VZWAVS \
+    VzwLcSilent \
+    VzwOmaTrigger \
+    WfcActivation
+
+# Symlinks
+PRODUCT_PACKAGES += \
+    libdmengine.so \
+    libdmjavaplugin.so
+
---
+++ sailfish/sailfish-vendor-blobs.mk
@@ -0,0 +1,52 @@
+# Copyright (C) 2017 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/google_devices/sailfish/proprietary/bin/mct-unit-test-app:system/bin/mct-unit-test-app \
+    vendor/google_devices/sailfish/proprietary/bin/nanotool:system/bin/nanotool \
+    vendor/google_devices/sailfish/proprietary/bin/pktlogconf:system/bin/pktlogconf \
+    vendor/google_devices/sailfish/proprietary/bin/qmi_simple_ril_test:system/bin/qmi_simple_ril_test \
+    vendor/google_devices/sailfish/proprietary/bin/test_bet_8996:system/bin/test_bet_8996 \
+    vendor/google_devices/sailfish/proprietary/bin/test_module_pproc:system/bin/test_module_pproc \
+    vendor/google_devices/sailfish/proprietary/etc/cne/Nexus/ATT/ATT_profiles.xml:system/etc/cne/Nexus/ATT/ATT_profiles.xml \
+    vendor/google_devices/sailfish/proprietary/etc/cne/Nexus/ROW/ROW_profiles.xml:system/etc/cne/Nexus/ROW/ROW_profiles.xml \
+    vendor/google_devices/sailfish/proprietary/etc/cne/Nexus/VZW/VZW_profiles.xml:system/etc/cne/Nexus/VZW/VZW_profiles.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.android.sdm.plugins.connmo.xml:system/etc/permissions/com.android.sdm.plugins.connmo.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.android.sdm.plugins.sprintdm.xml:system/etc/permissions/com.android.sdm.plugins.sprintdm.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.android.vzwomatrigger.xml:system/etc/permissions/com.android.vzwomatrigger.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.customermobile.preload.vzw.xml:system/etc/permissions/com.customermobile.preload.vzw.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.verizon.apn.xml:system/etc/permissions/com.verizon.apn.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.verizon.embms.xml:system/etc/permissions/com.verizon.embms.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.verizon.llkagent.xml:system/etc/permissions/com.verizon.llkagent.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.verizon.provider.xml:system/etc/permissions/com.verizon.provider.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/com.verizon.services.xml:system/etc/permissions/com.verizon.services.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/obdm_permissions.xml:system/etc/permissions/obdm_permissions.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/privapp-permissions-marlin.xml:system/etc/permissions/privapp-permissions-marlin.xml \
+    vendor/google_devices/sailfish/proprietary/etc/permissions/vzw_sso_permissions.xml:system/etc/permissions/vzw_sso_permissions.xml \
+    vendor/google_devices/sailfish/proprietary/etc/sysconfig/marlin_common.xml:system/etc/sysconfig/marlin_common.xml \
+    vendor/google_devices/sailfish/proprietary/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    vendor/google_devices/sailfish/proprietary/etc/sysconfig/whitelist_com.android.omadm.service.xml:system/etc/sysconfig/whitelist_com.android.omadm.service.xml \
+    vendor/google_devices/sailfish/proprietary/lib/libaptX_encoder.so:system/lib/libaptX_encoder.so \
+    vendor/google_devices/sailfish/proprietary/lib/libaptXHD_encoder.so:system/lib/libaptXHD_encoder.so \
+    vendor/google_devices/sailfish/proprietary/lib/libdmengine.so:system/lib/libdmengine.so \
+    vendor/google_devices/sailfish/proprietary/lib/libdmjavaplugin.so:system/lib/libdmjavaplugin.so \
+    vendor/google_devices/sailfish/proprietary/lib/libion.so:system/lib/libion.so \
+    vendor/google_devices/sailfish/proprietary/lib/libminui.so:system/lib/libminui.so \
+    vendor/google_devices/sailfish/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
+    vendor/google_devices/sailfish/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
+    vendor/google_devices/sailfish/proprietary/lib64/libminui.so:system/lib64/libminui.so \
+    vendor/google_devices/sailfish/proprietary/lib64/vendor.qti.atcmdfwd@1.0.so:system/lib64/vendor.qti.atcmdfwd@1.0.so \
+    vendor/google_devices/sailfish/proprietary/lib64/vendor.qti.qcril.am@1.0.so:system/lib64/vendor.qti.qcril.am@1.0.so \
+    vendor/google_devices/sailfish/proprietary/priv-app/VzwLcSilent/lib/arm/libmotricity.so:system/priv-app/VzwLcSilent/lib/arm/libmotricity.so
